6c0cbb2872536ddf47067bb3423300dac5a0c596
Ian James <ian.james@digital.cabinet-office.gov.uk>
2022-09-15 16:55:40 +0100

    Change the way the assets manifest is managed
    The list of components with a stylesheet needs to be able to be passed to the
    rendering application so it can be added to the list of things to be
    precompiled. This list is accessed by referencing
    `GovukPublishingComponents::AppHelpers::AssetHelper::COMPONENT_CSS_PATHS`.

--- lib/govuk_publishing_components/app_helpers/asset_helper.rb
+++ lib/govuk_publishing_components/app_helpers/asset_helper.rb
@@ -1,6 +1,87 @@
 module GovukPublishingComponents
   module AppHelpers
     class AssetHelper
+      # This is a list of all the components that have stylesheets - so a new
+      # component needs to be added to this list rather than being laboriously
+      # added and removed from every single rendering application.
+      COMPONENT_CSS_PATHS = %w[
+        accordion
+        action-link
+        attachment
+        attachment-link
+        back-link
+        big-number
+        breadcrumbs
+        button
+        cards
+        character-count
+        checkboxes
+        contents-list
+        contextual-guidance
+        contextual-sidebar
+        cookie-banner
+        copy-to-clipboard
+        date-input
+        details
+        devolved-nations
+        document-list
+        emergency-banner
+        error-alert
+        error-message
+        error-summary
+        feedback
+        fieldset
+        file-upload
+        govspeak
+        govspeak-html-publication
+        heading
+        hint
+        image-card
+        input
+        inset-text
+        intervention
+        inverse-header
+        label
+        layout-footer
+        layout-for-admin
+        layout-for-public
+        layout-header
+        layout-super-navigation-header
+        lead-paragraph
+        metadata
+        modal-dialogue
+        notice
+        organisation-logo
+        panel
+        phase-banner
+        previous-and-next-navigation
+        print-link
+        radio
+        related-navigation
+        reorderable-list
+        search
+        select
+        share-links
+        show-password
+        signup-link
+        single-page-notification-button
+        skip-link
+        step-by-step-nav
+        step-by-step-nav-header
+        step-by-step-nav-related
+        subscription-links
+        success-alert
+        summary-list
+        table
+        tabs
+        textarea
+        title
+        translation-nav
+        warning-text
+      ].map { |component|
+        "govuk_publishing_components/components/_#{component}.css"
+      }.freeze
+
       def initialize
         # Set up the lists of gem components, app components, and views.
         @gem_component_stylesheets_being_used = []
@@ -9,7 +90,7 @@ module GovukPublishingComponents
 
         # This list includes components already included in Static; taken from
         # https://github.com/alphagov/static/blob/198a598682df40ce4a2c3c286c06244297c18cf0/app/assets/stylesheets/application.scss
-
+        #
         # This is used to dedupe stylesheets without needing the full path.
         @stylesheet_list = %w[
           breadcrumbs
