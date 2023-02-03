62804df351e5313c03c2d222d56c9e7d864b6aa6
Martin Jones <martin.jones@digital.cabinet-office.gov.uk>
2023-02-03 09:28:15 +0000

    Refactor stylesheet list and add component methods
    The generic add_stylesheet_path method can be used to add any stylesheet path to the rendering application.
    
    The existing methods such as add_gem_component, are still in place as shorthand methods.
    
    The full path is now used in the stylesheet list to avoid any potential issues if a component with the same name is used in a different path.

--- lib/govuk_publishing_components/app_helpers/asset_helper.rb
+++ lib/govuk_publishing_components/app_helpers/asset_helper.rb
@@ -10,73 +10,68 @@ module GovukPublishingComponents
         "govuk_publishing_components/components/_#{component_name}.css"
       }.freeze
 
+      # This list includes components already included in Static; taken from
+      # https://github.com/alphagov/static/blob/198a598682df40ce4a2c3c286c06244297c18cf0/app/assets/stylesheets/application.scss
+
+      # This is used to dedupe stylesheets without needing the full path.
+      STYLESHEET_LIST = %w[
+        govuk_publishing_components/components/_breadcrumbs.css
+        govuk_publishing_components/components/_button.css
+        govuk_publishing_components/components/_error-message.css
+        govuk_publishing_components/components/_heading.css
+        govuk_publishing_components/components/_hint.css
+        govuk_publishing_components/components/_input.css
+        govuk_publishing_components/components/_label.css
+        govuk_publishing_components/components/_search.css
+        govuk_publishing_components/components/_skip-link.css
+        govuk_publishing_components/components/_textarea.css
+        govuk_publishing_components/components/_title.css
+
+        govuk_publishing_components/components/_cookie-banner.css
+        govuk_publishing_components/components/_feedback.css
+        govuk_publishing_components/components/_layout-footer.css
+        govuk_publishing_components/components/_layout-for-public.css
+        govuk_publishing_components/components/_layout-header.css
+        govuk_publishing_components/components/_layout-super-navigation-header.css
+      ].freeze
+
       def initialize
         # Set up the lists of gem components, app components, and views.
         @gem_component_stylesheets_being_used = []
         @app_component_stylesheets_being_used = []
         @view_stylesheets_being_used = []
+      end
 
-        # This list includes components already included in Static; taken from
-        # https://github.com/alphagov/static/blob/198a598682df40ce4a2c3c286c06244297c18cf0/app/assets/stylesheets/application.scss
-        #
-        # This is used to dedupe stylesheets without needing the full path.
-        @stylesheet_list = %w[
-          breadcrumbs
-          button
-          error-message
-          heading
-          hint
-          input
-          label
-          search
-          skip-link
-          textarea
-          title
-
-          cookie-banner
-          feedback
-          layout-footer
-          layout-for-public
-          layout-header
-          layout-super-navigation-header
-        ]
+      def add_stylesheet_path(component_path)
+        unless is_already_used?(component_path)
+          STYLESHEET_LIST << component_path
+        end
       end
 
       # Used to add a component that exists in the gem to the list
-      def add_gem_component(component)
-        unless is_already_used?(component)
-          @gem_component_stylesheets_being_used << "govuk_publishing_components/components/_#{component}.css"
-          @stylesheet_list << component
-        end
+      def add_gem_component(gem_component)
+        add_stylesheet_path("govuk_publishing_components/components/_#{gem_component}.css")
       end
 
       # Used to add a component that exists in the application to the list
-      def add_component(component)
-        unless is_already_used?(component)
-          @app_component_stylesheets_being_used << "components/_#{component}.css"
-          @stylesheet_list << component
-        end
+      def add_component(app_component)
+        add_stylesheet_path("components/_#{app_component}.css")
       end
 
       # Some applications have view-specific stylesheets - use this method
       # to add them to the list
-      def add_view(view)
-        unless is_already_used?(view)
-          @view_stylesheets_being_used << "views/_#{view}.css"
-          @stylesheet_list << view
-        end
+      def add_view(view_component)
+        add_stylesheet_path("views/_#{view_component}.css")
       end
 
       def get_stylesheets
-        @gem_component_stylesheets_being_used.sort +
-          @app_component_stylesheets_being_used.sort +
-          @view_stylesheets_being_used.sort
+        STYLESHEET_LIST
       end
 
     private
 
       def is_already_used?(component)
-        @stylesheet_list.include?(component)
+        STYLESHEET_LIST.include?(component)
       end
     end
   end
